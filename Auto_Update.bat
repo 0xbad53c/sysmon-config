02a32efeca4099610d2e76eff63143d2f906f48f
0xbad53c <74974697+0xbad53c@users.noreply.github.com>
2021-11-28 15:43:56 +0100

    Update Auto_Update.bat

--- Auto_Update.bat
+++ Auto_Update.bat
@@ -1,5 +1,5 @@
 @echo on
 cd C:\ProgramData\sysmon\
 @powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/0xbad53c/sysmon-config/master/sysmonconfig-export.xml','C:\Windows\sysmon-config.xml')"
-sysmon64 -c sysmonconfig-export.xml
+C:\Windows\sysmon.exe -c C:\Windows\sysmon-config.xml
 exit
